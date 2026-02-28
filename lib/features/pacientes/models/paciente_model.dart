ba232f98b54cd8aed996d61ea703fe464ab93273
JimmyJijon <jajijon2@tes.edu.ec>
2026-02-28 11:19:42 -0500

    Se corrige PacienteModel a versión final correcta

--- lib/features/pacientes/models/paciente_model.dart
+++ lib/features/pacientes/models/paciente_model.dart
@@ -1,30 +1,3 @@
-/*
-class PacienteModel {
-  final String nombre;
-  final String cedula;
-  final String telefono;
-  final String estado;
-
-  PacienteModel({
-    required this.nombre,
-    required this.cedula,
-    required this.telefono,
-    required this.estado,
-  });
-
-  // Esto te servirá luego para conectar con una base de datos
-  factory PacienteModel.fromMap(Map<String, String> map) {
-    return PacienteModel(
-      nombre: map['nombre'] ?? '',
-      cedula: map['cedula'] ?? '',
-      telefono: map['telefono'] ?? '',
-      estado: map['estado'] ?? 'Activo',
-    );
-  }
-}*/
-
-// lib/features/pacientes/models/paciente_model.dart
-
 class PacienteModel {
   final int idPaciente;
   final String cedula;
@@ -72,3 +45,4 @@ class PacienteModel {
     };
   }
 }
+
